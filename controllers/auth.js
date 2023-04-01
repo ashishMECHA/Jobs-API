a78c63d9337b9120650a6f796152c3ad700eeb01
Ashish Thakur <thakurashish525@gmail.com>
2023-04-02 02:09:20 +0530

    register api created

---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const BadRequestError = require("../errors/bad-request");
+
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+    },
+    token,
+  });
+};
+
+const login = async (req, res) => {
+  res.send("Login user");
+};
+
+module.exports = {
+  register,
+  login,
+};
